22a7f355f0a2ad385b4f26de79ff56174eb27295
HiggloH <Hilding.hammarback@gmail.com>
2021-01-09 12:41:45 +0100

    Fixed enemy shooting

--- Assets/Skripts/Enemy/EnemyShooting.cs
+++ Assets/Skripts/Enemy/EnemyShooting.cs
@@ -6,40 +6,45 @@ public class EnemyShooting : MonoBehaviour
 {
     public Transform firePoint;
     public GameObject EnemyBullet;
-    private float secondsBetweenShoot;
-
+    private float timeLeft;
+    private float maxTime;
 
     private void Start()
     {
         if(Difficulty.diff == 0)
         {
-            secondsBetweenShoot = 3;
+            maxTime = 3;
         }
 
         if (Difficulty.diff == 1)
         {
-            secondsBetweenShoot = 2;
+            maxTime = 2;
         }
 
         if (Difficulty.diff == 2)
         {
-            secondsBetweenShoot = 1;
+            maxTime = 1;
         }
 
-        StartCoroutine("WhenToShoot");
+        timeLeft = maxTime;
+    }
+
+    private void Update()
+    {
+        if(timeLeft > 0)
+        {
+            timeLeft -= Time.deltaTime;
+        }
+        else
+        {
+            Shoot();
+            timeLeft = maxTime;
+        }
+        
     }
 
     private void Shoot()
     {
         Instantiate(EnemyBullet, firePoint.position, firePoint.rotation);
-    }
-
-    IEnumerable WhenToShoot()
-    {
-        while (true)
-        {
-            yield return new WaitForSeconds(secondsBetweenShoot);
-            Shoot();
-        }
     }
 }
